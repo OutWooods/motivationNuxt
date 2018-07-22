d99531e9defa7429be560d1f6738220947dd4601
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2018-07-22 13:58:28 +0100

    add random coloured square

--- components/Pixl.vue
+++ components/Pixl.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    This should appear when working.
+  <div class="pixl" v-bind:style="{ backgroundColor: colourMaker() }">
   </div>
 </template>
 
+<style>
+  .pixl {
+    height: 10px;
+    width: 10px;
+  }
+</style>
+
+
 <script>
   export default {
+    created: function () {
+      this.colourMaker();
+    },
 
+    methods: {
+      colourMaker: () => {
+        return `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+      }
+    }
   }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-      <div id="box"></div>
-      <button @click="newPixl">Click me</button>
+    <button @click="newPixl">Click me</button>
+    <div id="pixl-box"></div>
   </div>
 </template>
-
-<style>
-</style>
 
 <script>
 
@@ -18,17 +15,13 @@
       Pixl
     },
 
-    mounted() {
-      console.log(this.$refs.box);
-    },
-
     methods: {
       newPixl: () => {
         const PixlConstructor = Vue.extend(Pixl);
         var newPixl = new PixlConstructor();
         newPixl.$mount();
 
-        document.getElementById('box').appendChild(newPixl.$el);
+        document.getElementById('pixl-box').appendChild(newPixl.$el);
       }
     }
   }
