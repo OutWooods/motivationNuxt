52986fdc47028ddb24f29cb6894b8bc8bdfadb47
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2018-07-24 08:09:37 +0100

    stash new stuff

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,22 @@
     <h1 class="text-center pb-6"> The Great Motivational Pixl's Site</h1>
     <div class="container">
       <div id="pixl-box"></div>
-      <div class="targets">
-        <p class="pl-6 pr-6">Spent 30mins doing coding</p>
-        <button @click="newPixl"
-                class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">
-          Target Achieved
-        </button>
-      </div>
+      <li id="targets">
+        <ul>
+          <p class="pl-6 pr-6">Spent 30mins doing coding</p>
+          <button @click="newPixl"
+                  class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">
+            Target Achieved
+          </button>
+        </ul>
+        <ul>
+          <p class="pl-6 pr-6"> Add new target</p>
+          <button @click="newTarget"
+                  class="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded">
+            Add New Target
+          </button>
+        </ul>
+      </li>
     </div>
   </div>
 </template>
@@ -25,11 +34,6 @@
     flex-wrap: wrap;
     width: 50%;
   }
-
-  .targets {
-    display: flex;
-  }
-
 </style>
 
 <script>
@@ -49,6 +53,16 @@
         newPixl.$mount();
 
         document.getElementById('pixl-box').appendChild(newPixl.$el);
+      },
+
+      newTarget: () => {
+        console.log(document.getElementById('pixl-box'));
+       console.log(document.getElementById('targets'));
+       const div = document.createElement('ul');
+       div.innerHTML = 'target2';
+       const targets = document.getElementById('targets');
+       const secondLastNode = targets.childNodes.length -1;
+       targets.insertBefore(div, targets.childNodes[secondLastNode]);
       }
     }
   }
